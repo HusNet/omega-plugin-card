fcf9626f715b8b297ef0b8695e36e4a85715a384
gaehus <huser.gaetan@hotmail.com>
2021-05-28 11:19:28 +0200

    display only isset

--- resources/views/overt/display.blade.php
+++ resources/views/overt/display.blade.php
@@ -1,11 +1,17 @@
 <div id="{{ preg_replace("/[\s_]/", "-", preg_replace("/[\s-]+/", " ", preg_replace("/[^a-z0-9_\s-]/", "", strtolower($header)))) }}" class="card {{ @$cardClass }}">
-	<div class="card-header {{ @$headerClass }}">
-		{{ @$header }}
-	</div>
-	<div class="card-body {{ @$contentClass }}">
-		{{ @$content }}
-	</div>
-	<div class="card-footer {{ @$footerClass }}">
-		{{ @$footer }}
-	</div>
+	@isset($header)
+		<div class="card-header {{ @$headerClass }}">
+			{{ @$header }}
+		</div>
+	@endisset
+	@isset($content)
+		<div class="card-body {{ @$contentClass }}">
+			{{ @$content }}
+		</div>
+	@isset($footer)
+	@endisset
+		<div class="card-footer {{ @$footerClass }}">
+			{{ @$footer }}
+		</div>
+	@endisset
 </div>
